6aebdfd22de7132d62e9f47bbc94badc46817656
richardjtelford <richard.telford@bio.uib.no>
2017-06-07 20:41:03 +0200

    tidying code, labels

--- expectations.R
+++ expectations.R
@@ -5,9 +5,9 @@ library("vegan")
 #data
 data(BCI)
 
-BC_1 <- colSums(BCI)
 
-#distribution of last digits
+
+#distribution of last digits in full dataset
 BCI %>% 
   gather() %>% 
   filter(value > 0) %>% 
@@ -18,12 +18,14 @@ BCI %>%
   ggplot(aes(x = value)) + 
     geom_bar()
 
+
 #effect of limited taxonomic breadth and count size
-BC_low <- sort(BC_1, decreasing = TRUE)[1:20]
+BC_1 <- colSums(BCI)
+BC_low <- sort(BC_1, decreasing = TRUE)[1:25]
 
 sizes <- c(50, 100, 200, 500)
 
-plyr::ldply(list(full = BC_1, low = BC_20), function(dataset){
+plyr::ldply(list(full = BC_1, low = BC_low), function(dataset){
   plyr::ldply(setNames(sizes, sizes), function(size){
    rmultinom(1000, size = size, prob = dataset) %>% 
       as.data.frame() %>% 
@@ -40,4 +42,5 @@ plyr::ldply(list(full = BC_1, low = BC_20), function(dataset){
   ggplot(aes(x = value, y = n, colour = countSize, group = countSize)) + 
   geom_line()+
   geom_point() +
-  facet_wrap(~resolution)
+  facet_wrap(~resolution) + 
+  labs(x = "Last digit", y = "Frequency", colour = "Count size")
