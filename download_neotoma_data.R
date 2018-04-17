2dbe4691e508b0049aadfeea6595afd493090131
richardjtelford <richard.telford@bio.uib.no>
2018-04-17 16:53:40 +0200

    joining depositional info to sites_meta

--- download_neotoma_data.R
+++ download_neotoma_data.R
@@ -22,13 +22,21 @@ sites <- europe %>%
 #extract site information
 sites_meta <- sites %>% 
   map("site.data") %>% 
-  map_df(as_data_frame)
+  map_df(as_data_frame) %>% 
+  bind_cols(sites %>% 
+              map_df("dataset.meta"))
 
 
 
 #download_data
 european_data <- sites %>% get_download()
-save(european_data, file = "european.Rdata")
+
+### download environment
+DepEnvtTypes <- get_table("DepEnvtTypes")
+CollectionUnits <- get_table("CollectionUnits")
+
+## save downloaded data
+save(sites, european_data, DepEnvtTypes, CollectionUnits, file = "european.Rdata")
 
 
 age_control <- european_data %>% 
@@ -37,6 +45,7 @@ age_control <- european_data %>%
     x %>% summarise(age_min = min(age), age_max = max(age), n = n(), res = n/(age_max - age_min) * 1000)
     },
     .id = "dataset.id") %>% 
+  mutate(dataset.id = as.numeric(dataset.id)) %>% 
   as_tibble()
 
 age_control <- age_control %>% mutate(length = case_when(
@@ -47,11 +56,10 @@ age_control <- age_control %>% mutate(length = case_when(
   TRUE ~"part Holocene"
 ))
   
-sites_meta <- sites_meta %>% bind_cols(age_control)
-##TODO - get dataset.id into site meta and left_join
+sites_meta <- sites_meta %>% inner_join(age_control)
 
 #map sites
-sites_meta %>% 
+g <- sites_meta %>% 
 ggplot(aes(x = long, y = lat, colour = length)) +
   geom_map(data = mp, map = mp, aes(map_id = region), inherit.aes = FALSE, fill = "grey80") +
   geom_rect(data = europe$loc %>% 
@@ -62,10 +70,32 @@ ggplot(aes(x = long, y = lat, colour = length)) +
   coord_quickmap() +
   scale_y_continuous(expand = c(0.01, 0))
 
+g
+
+## get environment data
+DepEnvtTypes <- DepEnvtTypes %>% 
+  left_join(DepEnvtTypes, by = c("DepEnvtHigherID" = "DepEnvtID"), suffix = c("", ".1")) %>% 
+  left_join(DepEnvtTypes, by = c("DepEnvtHigherID.1" = "DepEnvtID"), suffix = c("", ".2")) %>%
+  left_join(DepEnvtTypes, by = c("DepEnvtHigherID.2" = "DepEnvtID"), suffix = c("", ".3")) %>% 
+  left_join(DepEnvtTypes, by = c("DepEnvtHigherID.3" = "DepEnvtID"), suffix = c("", ".4")) %>% 
+  select(-starts_with("RecDate")) %>% 
+  as_tibble()
+
+CollectionUnits <- CollectionUnits %>% 
+  as_tibble() %>% 
+  left_join(DepEnvtTypes, by = "DepEnvtID") %>% 
+  select(-starts_with("RecDate"))
 
 
-x <- get_download(sites[[1]])
-ages(x)
-x %>% neotoma:::Stratiplot.download(yaxis = "depth")
+## merge depo environement with  site_meta
+sites_meta <- sites_meta %>% 
+  left_join(CollectionUnits, by = c("collection.handle" = "Handle"))
 
-x
+g %+% (sites_meta %>% filter(length == "Most Holocene")) +
+         aes(colour = DepEnvt.4)
+
+# x <- get_download(sites[[1]])
+# ages(x)
+# x %>% neotoma:::Stratiplot.download(yaxis = "depth")
+# 
+# x
