d1a4aaa5989b3ba90cc87ba0659d1f5b8ad35555
richardjtelford <richard.telford@bio.uib.no>
2018-04-24 20:43:17 +0200

    automatically open pdf (at least on linux)

--- make.R
+++ make.R
@@ -3,13 +3,16 @@ library("tidyverse")
 library("neotoma")
 
 #render wrapper
-produce_regional_pollen_diagrams <- function(region, ...){
+produce_regional_pollen_diagrams <- function(region, ..., open_pdf = FALSE){
   rmarkdown::render(
     input = "pollen_diagrams.Rmd", 
     params = list(region = region), 
     ...
   )
+  if(isTRUE(open_pdf)){
+    system("evince pollen_diagrams.pdf")
+  }
 }
 
 scandinavia <-  data_frame(name = "Scandinavia", lonW = 4, latS = 55, lonE = 37, latN = 81)
-produce_regional_pollen_diagrams(scandinavia)
+produce_regional_pollen_diagrams(scandinavia, open_pdf = TRUE)
