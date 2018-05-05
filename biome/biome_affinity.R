81ca1bd6d67ab33f844d3e795df233c2633de8ef
richardjtelford <richard.telford@bio.uib.no>
2018-05-05 12:42:36 +0200

    initial commit

---
+++ biome/biome_affinity.R
@@ -0,0 +1,24 @@
+#biome
+
+library("tidyverse")
+
+#load pollen
+
+#load pollen-pft dictionary
+
+#load pft biome assignment
+
+
+pollen %>% 
+  gather(key = taxon, value = percent, -sampleID) %>%
+  filter(percent > 0.5) %>% 
+  left_join(pollen_pft) %>% 
+  group_by(sampleID, pft) %>% 
+  summarise(pft_sum = sum(sqrt(percent))) %>% 
+  left_join(pft_biome) %>% 
+  group_by(sampleID, biome) %>% 
+  summarise(affinity = sum(pft_sum)) %>% 
+  arrange(desc(affinity))
+
+
+  summarise()
